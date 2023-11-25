65fcd5c8590c209523d01c446c637cde9f5f9e3d
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-11-25 13:47:16 -0500

    Added login REST API and created a middlewear which checks the JWT token

--- MVC/Controller/userController.js
+++ MVC/Controller/userController.js
@@ -1,5 +1,8 @@
 const User = require("../Model/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const SECRETKEY = "thisIsATempSecretKeyIAmUsingForTheBackEndApplication";
 
 const saltRounds = 10;
 
@@ -34,6 +37,55 @@ const signupUser = async (req, res, next) => {
   }
 };
 
+const loginUser = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const loggedInUser = await User.findOne({ email });
+
+    const decodedPassword = await bcrypt.compare(
+      password,
+      loggedInUser.password
+    );
+
+    if (!decodedPassword) {
+      return res.status(401).json({ message: "Password does not match" });
+    }
+
+    const token = jwt.sign(
+      {
+        firstName: loggedInUser.firstName,
+        userId: loggedInUser._id,
+      },
+      SECRETKEY,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({ message: "User logged in", token });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const protectedRoute = async (req, res, next) => {
+  const user = req.user;
+  const welcomeUser = await User.findById(user);
+
+  if (!welcomeUser) {
+    return res.status(500).json("Internal Server Error");
+  }
+
+  return res
+    .status(200)
+    .json({
+      message: `Welcome ${welcomeUser.firstName} to the protected route`,
+    });
+};
+
 module.exports = {
   signupUser,
+  loginUser,
+  protectedRoute
 };
--- MVC/Routes/router.js
+++ MVC/Routes/router.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controller/userController");
+const isAuthorised = require("../Middlewear/jwtHelper");
 
 router.post("/signup", userController.signupUser);
+router.post("/login", userController.loginUser);
+router.get("/protected-route", isAuthorised, userController.protectedRoute);
 
 module.exports = router;
