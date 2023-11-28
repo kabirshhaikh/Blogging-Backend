7e19c9944e9d0158d4855d8bdad626e1408481d5
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-11-28 18:31:12 -0500

    Implemented Multer as a middlewear and added picture functionality in create new post and update the post

--- MVC/Model/Posts.js
+++ MVC/Model/Posts.js
@@ -15,6 +15,7 @@ const postSchema = new Schema({
     ref: "User",
     required: true,
   },
+  postPicture: String,
   createdAt: {
     type: Date,
     default: Date.now(),
--- MVC/Routes/router.js
+++ MVC/Routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../Controller/userController");
 const postsController = require("../Controller/postsController");
 const isAuthorised = require("../Middlewear/jwtHelper");
+const multer = require("../Middlewear/Multer");
 
 //User Routes:
 router.post("/signup", userController.signupUser);
@@ -10,8 +11,18 @@ router.post("/login", userController.loginUser);
 router.get("/protected-route", isAuthorised, userController.protectedRoute);
 
 //Post's Routes:
-router.post("/create-post", isAuthorised, postsController.createPost);
+router.post(
+  "/create-post",
+  isAuthorised,
+  multer.single("postPicture"),
+  postsController.createPost
+);
 router.delete("/delete-post/:id", isAuthorised, postsController.deletePost);
-router.patch("/update-post/:id", isAuthorised, postsController.updatePost);
+router.patch(
+  "/update-post/:id",
+  isAuthorised,
+  multer.single("postPicture"),
+  postsController.updatePost
+);
 
 module.exports = router;
